2d6e521b17350275c4aa0b01b54266aead29ebc7
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-03-31 18:15:58 +0300

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+  const invite = [];
+  for (let i = 0; i < name.length; i++) {
+    invite.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return invite;
+}
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
